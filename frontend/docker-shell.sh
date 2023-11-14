432e3f06d2094bb4be565c3729eedd3b808fd77b
Shivas Jayaram <shivasj@gmail.com>
2023-11-14 17:24:33 -0500

    fixed docker shell

--- frontend/docker-shell.sh
+++ frontend/docker-shell.sh
@@ -12,5 +12,5 @@ docker build -t $IMAGE_NAME -f Dockerfile.dev .
 
 # Run the container
 docker run --rm --name $IMAGE_NAME -ti \
---mount type=bind,source=$BASE_DIR,target=/app \
+--mount type=bind,source="$BASE_DIR",target=/app \
 -p 3000:3000 $IMAGE_NAME
