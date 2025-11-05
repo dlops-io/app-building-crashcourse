36bfaa66d6cda81f6e2dcc3dfafee4a6a870aacf
shivasj <shivasj@gmail.com>
2025-11-05 07:26:05 -0500

    updated configs

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -8,31 +8,6 @@ const nextConfig = {
         });
         return config;
     },
-    rewrites: async () => {
-        return [
-            {
-                source: "/api/:path*",
-                destination:
-                    process.env.NODE_ENV === "development"
-                        ? "http://rag-system-api-service:9000/:path*"
-                        : "/api/",
-            },
-            {
-                source: "/docs",
-                destination:
-                    process.env.NODE_ENV === "development"
-                        ? "http://rag-system-api-service:9000/docs"
-                        : "/api/docs",
-            },
-            {
-                source: "/openapi.json",
-                destination:
-                    process.env.NODE_ENV === "development"
-                        ? "http://rag-system-api-service:9000/openapi.json"
-                        : "/api/openapi.json",
-            },
-        ];
-    },
     reactStrictMode: false,
 };
 
